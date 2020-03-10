2d9a45e081003f5c052a96244a189014beead9a2
yogymax <yogymax@gmail.com>
2020-03-10 10:50:41 +0530

    Push to master directly

--- src/main/java/com/scp/pckg/add/Sample.java
+++ src/main/java/com/scp/pckg/add/Sample.java
@@ -3,5 +3,6 @@ package com.scp.pckg.add;
 public class Sample {
 		public static void main(String[] args) {
 			System.out.println("inside sample..");
+			System.out.println("Added one more change..");
 		}
 }
