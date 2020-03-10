0ccb71dd12f3afb6ad09f94e56f5670f3dc189e2
yogymax <yogymax@gmail.com>
2020-03-10 11:05:16 +0530

    local commit

--- src/main/java/com/scp/pckg/add/Sample.java
+++ src/main/java/com/scp/pckg/add/Sample.java
@@ -5,7 +5,6 @@ public class Sample {
 			System.out.println("inside sample..");
 			System.out.println("Added one more change..");
 			System.out.println("Added one more change..added by another developer...");
-			System.out.println("inside sampel dev2");
-			System.out.println("inside sampel dev1");
+			
 		}
 }
